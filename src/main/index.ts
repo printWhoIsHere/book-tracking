9cfe76f18571c6ea19ef119a379851af0b6b2af7
printWhoIsHere <uvarov_2011@list.ru>
2025-04-16 05:54:49 +0300

    Update

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,10 @@
-import { app, shell, BrowserWindow } from 'electron';
-import { join } from 'path';
-import { electronApp, optimizer, is } from '@electron-toolkit/utils';
-import icon from '../../resources/icon.png?asset';
+import { app, shell, BrowserWindow } from 'electron'
+import { join } from 'path'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import icon from '../../resources/icon.png?asset'
 
-import './settings';
-import './ipc';
+import './settings'
+import './ipc'
 
 function createWindow(): void {
 	const mainWindow = new BrowserWindow({
@@ -19,40 +19,40 @@ function createWindow(): void {
 			preload: join(__dirname, '../preload/index.js'),
 			sandbox: false,
 		},
-	});
+	})
 
 	mainWindow.on('ready-to-show', () => {
-		mainWindow.show();
-	});
+		mainWindow.show()
+	})
 
 	mainWindow.webContents.setWindowOpenHandler((details) => {
-		shell.openExternal(details.url);
-		return { action: 'deny' };
-	});
+		shell.openExternal(details.url)
+		return { action: 'deny' }
+	})
 
 	if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-		mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
+		mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
 	} else {
-		mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
+		mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
 	}
 }
 
 app.whenReady().then(() => {
-	electronApp.setAppUserModelId('com.electron');
+	electronApp.setAppUserModelId('com.electron')
 
 	app.on('browser-window-created', (_, window) => {
-		optimizer.watchWindowShortcuts(window);
-	});
+		optimizer.watchWindowShortcuts(window)
+	})
 
-	createWindow();
+	createWindow()
 
 	app.on('activate', function () {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
-});
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
+})
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
-		app.quit();
+		app.quit()
 	}
-});
+})
