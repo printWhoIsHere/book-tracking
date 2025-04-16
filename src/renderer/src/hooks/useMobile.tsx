ca92b1b01299b5a25ee359be615304678d0b5ee1
printWhoIsHere <uvarov_2011@list.ru>
2025-04-16 07:25:33 +0300

    Modify

--- src/renderer/src/hooks/useMobile.tsx
+++ src/renderer/src/hooks/useMobile.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 const MOBILE_BREAKPOINT = 768
 
-export function useMobile() {
+function useMobile() {
 	const [isMobile, setIsMobile] = useState<boolean>(() =>
 		typeof window !== 'undefined'
 			? window.innerWidth < MOBILE_BREAKPOINT
@@ -24,3 +24,5 @@ export function useMobile() {
 
 	return isMobile
 }
+
+export { useMobile }
