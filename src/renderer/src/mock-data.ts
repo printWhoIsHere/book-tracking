9117c86188984f6df4ed615e50126a6ba637d986
printWhoIsHere <uvarov_2011@list.ru>
2025-04-16 21:46:53 +0300

    Update

--- src/renderer/src/mock-data.ts
+++ src/renderer/src/mock-data.ts
@@ -1,5 +1,1 @@
-export const profiles = [
-	{ name: 'Profile 1', quantity: '107' },
-	{ name: 'Profile 2', quantity: '28' },
-	{ name: 'Profile 3', quantity: '0' },
-]
+export const profiles = [{ name: 'default', quantity: '000' }]
